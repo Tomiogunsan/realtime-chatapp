c41ce399e6fcad2f4309701332cc3a5196483ded
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-01 10:16:57 +0100

    installed socket.io and set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import {io } from "socket.io-client";
 import "./App.css";
 
 const socket = io('ws://localhost:4000') 
+socket.on("connect", () => {
+  console.log('Connected to server')
+} )
 
 function App() {
   return <div className="App"></div>;
