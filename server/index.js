c50c7ac6d77f7b45308fe164c3d50c01ad09ab17
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-01 10:10:13 +0100

    installed socket.io and set up

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const app = express()
+const { Server } = require("socket.io");
+
+app.use(cors())
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on('connection', (socket) => {
+    console.log("new user connected");
+})
+
+app.get('/', (req,res) => {
+    res.send('Hello world')
+})
+server.listen(4000, () => 
+console.log("Server is running on port 3000")
+);
