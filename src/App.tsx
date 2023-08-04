ef3ed0645e0e1b317e504fa117a7cbf4652db778
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-04 11:36:18 +0100

    implemted home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import {io } from "socket.io-client";
+import { io } from "socket.io-client";
 import "./App.css";
 import InputForm from "./components/InputForm";
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const socket = io('ws://localhost:4000') 
+const socket = io("ws://localhost:4000");
 socket.on("connect", () => {
-  console.log('Connected to server')
-} )
+  console.log("Connected to server");
+});
 
-socket.on('newEmail', () => {
+socket.on("newEmail", () => {
   console.log("New email");
-})
+});
 
 function App() {
-  return <div className="App">
-
-    <InputForm socket={socket}/>
-  </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path='/form' element={<InputForm />}/>
+      </Routes>
+        <InputForm socket={socket} />
+      
+    </Router>
+  );
 }
 
 export default App;
