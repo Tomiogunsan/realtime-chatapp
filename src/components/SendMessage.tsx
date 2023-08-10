3c49458ebef8ca910b0327f07485010575b4f30b
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-10 11:30:10 +0100

    implemented sendMessage component

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -7,27 +7,23 @@ type Props = {
 }
 
 export default function SendMessage({socket, displayName, group}: Props) {
-     const [val, setVal] = useState({
-       message: "",
-     });
-     const{ message} = val
+     const [msg, setMsg] = useState('');
+     
      function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-       setVal((state) => ({
-         ...state,
-         [e.target.name]: e.target.value,
-       }));
+      setMsg(e.target.value)
      }
 
      function sendMessage(){
-      if(message !== ''){
+      if(msg !== ''){
         const createdtime = Date.now()
         socket.emit('sendMessage', {
-          message,
+          msg,
           displayName,
           createdtime,
           group
 
         })
+        setMsg('')
       }
      }
   return (
@@ -35,11 +31,11 @@ export default function SendMessage({socket, displayName, group}: Props) {
       <input
         type="text"
         name="message"
-        value={val.message}
+        value={msg}
         onChange={onInputChange}
       />
       <button className="ml-6 px-4 bg-slate-500 text-white">Send</button>
-      <button className="px-4 bg-pink-600 text-white">Send location</button>
+     
     </div>
   );
 }
