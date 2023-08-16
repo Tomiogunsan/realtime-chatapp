9a545bb8e65996c5887bf200e95b666320364a69
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-16 08:44:04 +0100

    ui design of register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,39 +3,43 @@ import Input from "../sharedComponent/input/Input";
 
 export default function Register() {
   return (
-    <div>
-      <Input
-        label="Firstname"
-        placeholder="Enter your firstname"
-        type="input"
-        name=""
-        // value={}
-        // onChange={}
-      />
-      <Input
-        label="Lastname"
-        placeholder="Enter your lastname"
-        type="input"
-        name=""
-        // value={}
-        // onChange={}
-      />
-      <Input
-        label="Email"
-        placeholder="Enter your email"
-        type="email"
-        name=""
-        // value={}
-        // onChange={}
-        />
-      <Input
-        label="Password"
-        placeholder="Enter your password"
-        type="password"
-        name=""
-        // value={}
-        // onChange={}
-      />
+    <div className=" bg-[#c1cbd8] h-[100vh] w-full py-28">
+      <div className=" mx-auto bg-white w-[550px] h-[450px] rounded-2xl shadow-2xl pt-16">
+        <div className="flex flex-col items-center justify-center">
+          <Input
+            label="Firstname"
+            placeholder="Enter your firstname"
+            type="input"
+            name=""
+            // value={}
+            // onChange={}
+          />
+          <Input
+            label="Lastname"
+            placeholder="Enter your lastname"
+            type="input"
+            name=""
+            // value={}
+            // onChange={}
+          />
+          <Input
+            label="Email"
+            placeholder="Enter your email"
+            type="email"
+            name=""
+            // value={}
+            // onChange={}
+          />
+          <Input
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+            name=""
+            // value={}
+            // onChange={}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/sharedComponent/input/Input.tsx
+++ src/sharedComponent/input/Input.tsx
@@ -5,7 +5,7 @@ export default function Input(props: InputProps) {
   const { label, placeholder, type, name, value, onChange } = props;
   return (
     <div className="flex flex-col">
-      <label className="text-base pb-2 font-bold text-[#808080]">{label}</label>
+      <label className="text-base pb-2 font-bold text-[#11141b]">{label}</label>
       <input
         placeholder={placeholder}
         type={type}
