6726086b5afd867bbea661da41b1933ee2c05606
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-25 12:26:50 +0100

    home group chat design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { toast } from "react-toastify";
+import Input from "../sharedComponent/input/Input";
+import Button from "../sharedComponent/button/button";
 
 type Props = {
   socket: any;
@@ -21,53 +23,54 @@ export default function Home({
   onChangeInput,
 }: Props) {
   const navigate = useNavigate();
-  let id = uuidv4();
-  console.log(id)
+
   function joinRoom() {
     if (displayName !== "" && group !== "") {
       socket.emit(
         "join",
 
-        { displayName, group, id }
+        { displayName, group }
       );
     }
     navigate("/chat");
   }
   return (
-    <div className="flex flex-col items-center justify-center mt-10 gap-4">
-      <h1>Join Room</h1>
+    <div className="flex flex-col items-center justify-center  gap-4  bg-[#c1cbd8] h-[100vh] w-full py-28">
+      <h1 className="font-bold text-[30px] text-[#055486] ">Join a Group</h1>
+      <form className=" mx-auto bg-[#ffffff] w-[550px] h-[450px] rounded-2xl shadow-2xl pt-16">
+        <div className="flex flex-col items-center justify-center">
+          <Input
+            name="displayName"
+            placeholder="Name"
+            value={displayName}
+            onChange={onChangeInput}
+          />
 
-      <input
-        name="displayName"
-        type="text"
-        placeholder="Name"
-        className="border"
-        value={displayName}
-        onChange={onChangeInput}
-      />
-
-      <select
-        className="border"
-        name="group"
-        value={group}
-        onChange={onChangeInput}
-        defaultValue="none"
-      >
-        {/* <option value="select course" disabled>
+          <select
+            className="w-[350px] mt-4 border outline-[#6d67ff] px-2 py-[10px] mb-2 rounded-md shadow-sm"
+            name="group"
+            value={group}
+            onChange={onChangeInput}
+            defaultValue="none"
+          >
+            {/* <option value="select course" disabled>
             --Select a course--
           </option> */}
 
-        {groupArray.map((list, i) => {
-          return <option key={i}>{list}</option>;
-        })}
-      </select>
-      <button
-        type="submit"
-        className="bg-pink-700 text-white px-4"
-        onClick={joinRoom}
-      >
-        Join
-      </button>
+            {groupArray.map((list, i) => {
+              return <option key={i}>{list}</option>;
+            })}
+          </select>
+          <Button
+            type="submit"
+            variant="primary"
+            className="mt-4 w-[60%] rounded-md font-semibold"
+            onClick={joinRoom}
+          >
+            Join
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
